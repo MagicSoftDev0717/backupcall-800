40927058e4140bdc328a58281aef1bb02e23721f
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-09-29 20:26:18 +0700

    update login/signup deploy

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -11,7 +11,7 @@ import bcrypt from "bcryptjs";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2023-10-16",
+  apiVersion: "2025-08-27.basil", // ✅ matches your installed stripe version types
 });
 
 export const authConfig: AuthOptions = {
