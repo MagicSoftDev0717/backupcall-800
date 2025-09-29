06d9a2da838506b931a3c2806c530ae73467b8da
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-09-29 20:29:44 +0700

    update prisma.ts any data type error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,20 @@
 // src/lib/prisma.ts (or /lib/prisma.ts)
+// import { PrismaClient } from "@prisma/client";
+
+// export const prisma = globalThis.prisma || new PrismaClient();
+// if (process.env.NODE_ENV !== "production") (globalThis as any).prisma = prisma;
+
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") (globalThis as any).prisma = prisma;
+declare global {
+  // allow global `var prisma` to survive hot reloads in development
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma = globalThis.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
+
